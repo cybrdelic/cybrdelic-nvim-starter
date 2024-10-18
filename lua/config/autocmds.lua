a542f6c3d8c08cd359ed178dfcdae8d3b9cf1a0e
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-10-17 18:07:52 -0700

    Add alpha-nvim: Auto-start dashboard on VimEnter; update header and buttons in alpha.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -19,3 +19,17 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = true, silent = true })
   end,
 })
+
+
+-- File: ~/.config/nvim/init.lua
+
+-- Add this near the end of the file
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Start Alpha when vim is opened with no arguments",
+  group = vim.api.nvim_create_augroup("alpha_autostart", { clear = true }),
+  callback = function()
+    if vim.fn.argc() == 0 and vim.api.nvim_buf_get_name(0) == "" then
+      require("alpha").start()
+    end
+  end,
+})
--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,18 +1,26 @@
+-- File: ~/.config/nvim/lua/plugins/alpha.lua
+
 return {
   "goolord/alpha-nvim",
-  opts = function()
+  priority = 900,
+  config = function()
     local dashboard = require("alpha.themes.dashboard")
-    local logo = [[
-      ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-      ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-      ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-      ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-      ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-      ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
-
-                         [ @elijahmanor ]
-    ]]
-    dashboard.section.header.val = vim.split(logo, "\n", { trimempty = true })
-    return dashboard.opts
+    dashboard.section.header.val = {
+      [[                               __                ]],
+      [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+      [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+      [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+      [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+      [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+    }
+    dashboard.section.buttons.val = {
+      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+      dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    }
+    require("alpha").setup(dashboard.config)
   end,
 }
